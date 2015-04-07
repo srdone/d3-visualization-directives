a79112275b22057c6549c2b84e66d606191ccdba
Stephen Done <srdone@gmail.com>
2015-04-06 21:15:05 -0700

    rough use of scale

--- components/scatterplot/vizScatterplotController.js
+++ components/scatterplot/vizScatterplotController.js
@@ -9,35 +9,66 @@
 
 		var vm = this;
 		var svg, datapoints;
+		var xScale, yScale;
+		var width = 500;
+		var height = 300;
 
 		activate();
 
 		function activate () {
 			svg = d3.select($element[0])
 				.append('svg')
-				.attr('width', 500)
-				.attr('height', 300);
+				.attr('width', width)
+				.attr('height', height);
+
+			xScale = d3.scale.linear().range([0, width]);
+			yScale = d3.scale.linear().range([height, 0]);
 
 			graphData();
 		};
 
-		function graphData () {	
+		function graphData () {
+			setScaleDomains();
+
 			datapoints = svg.selectAll('circle')
 				.data(vm.data);
 			
 			datapoints.enter()
 				.append('circle')
 				.attr('cx', function (d) {
-					return d.x;
+					return xScale(d.x);
 				})
 				.attr('cy', function (d) {
-					return d.y;
+					return yScale(d.y);
 				})
 				.attr('r', function (d) {
 					return d.radius;
 				});
 
 			datapoints.exit().remove();
+		};
+
+		function setScaleDomains () {
+			var xValues, yValues;
+			var xMin, xMax;
+			var yMin, yMax;
+
+			xValues = vm.data.map(function (current) {
+				return current.x;
+			});
+
+			yValues = vm.data.map(function (current) {
+				return current.y;
+			});
+
+			xMin = d3.min(xValues);
+			xMax = d3.max(xValues);
+
+			yMin = d3.min(yValues);
+			yMax = d3.max(yValues);
+
+			xScale.domain([xMin, xMax]);
+			yScale.domain([yMin, yMax]);
 		};
 
 		$scope.$watch('vm.data', graphData, true);
